790bba4790a3e8cad8430c89f1823c5c582c54a8
michael moore <michael.moore@hey.com>
2024-06-30 17:57:02 -0400

    Created initial api on front-end for testing

---
+++ notes-client/src/NoteAPI.js
@@ -0,0 +1,78 @@
+const apiEndpoint = "http://localhost:8000/api/notes";
+
+async function getNotes() {
+   const response = await fetch(apiEndpoint);
+   if (response.ok) {
+      return response.json();        
+   }
+   else {
+      console.log(response);
+      return null;
+   }
+}
+
+async function getNote(noteId) {
+   const response = await fetch(`${apiEndpoint}/${noteId}`);
+   if (response.ok) {
+      return response.json();       
+   }
+   else {
+      console.log(response);
+      return null;
+   }
+}
+
+async function addNote(note) {
+   const response = await fetch(apiEndpoint, {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+   })
+   if (response.ok) {
+      return response.body;       
+   }
+   else {
+      console.log(response);
+      return null;
+   }
+}
+
+async function editNote(noteId, notePart) {
+   const response = await fetch(`${apiEndpoint}/${noteId}`, {
+      method: "PUT",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(notePart),
+   })
+   if (response.ok) {
+      return response.body;
+   }
+   else {
+      console.log(response);
+      return null;
+   }
+}
+
+async function deleteNote(noteId) {
+   const response = await fetch(`${apiEndpoint}/${noteId}`, {
+      method: "DELETE"
+   });
+   if (response.ok) {
+      return response.body;
+   }
+   else {
+      console.log(response);
+      return null;
+   }
+}
+
+export {      
+   getNotes,
+   getNote,
+   addNote,
+   editNote,
+   deleteNote
+};
