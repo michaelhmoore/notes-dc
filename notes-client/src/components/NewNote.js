2f451047472c03f4926a5408d6d02856261281e5
michael moore <michael.moore@hey.com>
2024-07-01 17:40:17 -0400

    added form and change handling fon new notes

--- notes-client/src/components/NewNote.js
+++ notes-client/src/components/NewNote.js
@@ -1,16 +1,35 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { newNote } from '../NoteAPI';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
 function NewNote() {
   const [show, setShow] = useState(true);
+  const [inputs, setInputs] = useState({ title: '', noteText: ''});
+
   const navigate = useNavigate();
   const handleClose = () => {
     setShow(false);
     navigate(-1);
     //set up cancel/x route vs. save route, separate functions
   };
+  function handleChange(name, value) {
+    setInputs(values => ({ ...values, [name]: value }));
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const note = {
+      title: inputs.title,
+      text: inputs.noteText
+    };
+
+    await newNote(note);
+    navigate(-1);
+  }
   const handleShow = () => setShow(true);
   return (
     <>
@@ -18,16 +37,28 @@ function NewNote() {
         Create a new note
       </Button>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Title>note title</Modal.Title>
-        <Modal.Body>note body</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleClose}>
-            Close
-          </Button>      
-          <Button variant='primary' onClick={handleClose}>
-            Save note            
-          </Button>
-        </Modal.Footer>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className='mb-3' controlId='title'>
+            <Modal.Title>
+              <Form.Label>title</Form.Label>
+              <Form.Control type='text' value={inputs.title} onChange={(e) => handleChange('title', e.target.value)} />
+            </Modal.Title>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='noteText'>
+            <Modal.Body>
+              <Form.Label>noteText</Form.Label>
+              <Form.Control type='textarea' rows={3} value={inputs.noteText} onChange={(e) => handleChange('text', e.target.value)} />
+            </Modal.Body>
+          </Form.Group>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={handleClose}>
+              Close
+            </Button>      
+            <Button variant='primary' onClick={handleClose}>
+              Save note            
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>    
     </>
   );
