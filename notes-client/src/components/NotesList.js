ccb064591028fad07e55da177967ee3699b64a6f
michael moore <michael.moore@hey.com>
2024-07-01 14:23:32 -0400

    created NotesList component

--- notes-client/src/components/NotesList.js
+++ notes-client/src/components/NotesList.js
@@ -1,9 +1,40 @@
-import { LinkContainer } from 'react-router-bootstrap';
-import { Button, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import Spinner from 'react-bootstrap/Spinner';
+import Note from './Note';
+import { getNotes, deleteNote } from '../NoteAPI';
 
 function NotesList() {
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function deleteFromList(noteId) {
+    deleteNote(noteId);
+    setNotes(prevNotes => prevNotes.filter(
+      note => note._id !== noteId));
+  }
+
+  useEffect(() => {
+    async function getAllNotes() {
+      const notes = await getNotes();
+      setNotes(notes);
+      setIsLoading(false);
+    }
+
+    getAllNotes();
+  }, []);
+
   return (
-    <h1>Notes list component</h1>
+    <>
+      {isLoading
+        ? (
+          <Spinner animation='border' role='status'>
+          <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : notes.map(note => (
+          <Note key={note._id} value={note} delete={deleteFromList} />
+        ))
+      }
+    </>
   );
 }
 
