ddaa1278c6cec2b156b0723ee742b23a18b401af
michael moore <michael.moore@hey.com>
2024-07-02 16:24:23 -0400

    header update

--- notes-client/src/components/Header.js
+++ notes-client/src/components/Header.js
@@ -1,16 +1,50 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Button, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
 
 function Header() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  // create header that's changed based on authenitcation
+
+  function handleLogout() {
+    localStorage.removeItem('token');
+    navigate('/login');
+  }
+
   return (
     <Row>
+      <LinkContainer to='/'>
       <Col><h1>SimplyNoting</h1></Col>
-      <Col>
-        <LinkContainer to='/new'>
-          <Button className='float-end'>
-            New note
-          </Button>
-        </LinkContainer>
+      </LinkContainer>
+      <Col className='text-end'>
+        {token ? (
+          <>
+          <Button variant='secondary' onClick={handleLogout}>Logout</Button>
+            <LinkContainer to='/new'>
+              <Button className='ms-2 float-end'>
+                New note
+              </Button>
+            </LinkContainer>
+            <Button className='float-end me-2' onClick={handleLogout}>
+              Logout
+            </Button>
+          </>
+        ) : (
+          <>
+            <LinkContainer to='/login'>
+              <Button className='ms-2 float-end'>
+                Login
+              </Button>
+            </LinkContainer>
+            <LinkContainer to='/register'>
+              <Button className='ms-2'>
+                Register
+              </Button>
+            </LinkContainer>
+          </>
+        )}
       </Col>
     </Row>
   );
